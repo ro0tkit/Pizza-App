db0f62f912d5285967483c0735c82d35f0893598
Luther <lutherking30@gmail.com>
2021-09-20 10:35:46 +0300

    Added autoselect flavor function

--- js/main.js
+++ js/main.js
@@ -1,6 +1,29 @@
 $(document).ready(function() {
     // Auto-selects pizza order on form
     $('#btn1').click(function() {
-        $('#toppings').val('Cheese');
+        $('#flavor').val('vegparadise');
+      });
+      $('#btn2').click(function() {
+        $('#flavor').val('chickentikka');
+      });
+      $('#btn3').click(function() {
+        $('#flavor').val('ham');
+      });
+      $('#btn4').click(function() {
+        $('#flavor').val('bbq');
+      });
+      $('#btn5').click(function() {
+        $('#flavor').val('creamy');
+      });
+      $('#btn6').click(function() {
+        $('#flavor').val('cheesy');
       });
 });
+
+/*------Modal--------------
+const toggleModal = () => {
+  document.querySelector(".modal")
+  .classList.toggle("modal-hidden");
+};
+document.querySelector("#show-modal")
+.addEventListener("click", toggleModal );*/
